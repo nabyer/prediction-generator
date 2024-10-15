8dc97dff59e6d07da70d6a0efddab7fef2c6e9cc
Nabil Yermani <nabilyermani@yahoo.de>
2024-10-15 10:01:36 +0200

    Adding the IAM terraform module.

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_role" "lambda_execution_role" {
+    name = var.role_name
+
+    assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Principal": {
+                "Service": [
+                    "lambda.amazonaws.com"
+                ]
+            }
+        }
+    ]
+})
+
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_execution_role_policy_attachment" {
+    role = aws_iam_role.lambda_execution_role.name
+    policy_arn = "arn:aws:iam:aws:policy/service-role/${var.policy_name}"
+    
+}
---
+++ terraform/modules/iam/variables.tf
@@ -0,0 +1,9 @@
+variable "role_name" {
+    description = "The name of the Lambda execution role."
+    type = string
+}
+
+variable "policy_name" {
+    description = "The name of the AS managed Lambda execution role basec policy."
+    type = string
+}
