4e3c5e630cd3ffc130094810be48ff80f4865bf1
Nabil Yermani <nabilyermani@yahoo.de>
2024-10-15 12:35:00 +0200

    Adding github actions workflow to deploy the lambda function.

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -17,11 +17,11 @@ resource "aws_iam_role" "lambda_execution_role" {
         }
     ]
 })
-
+  
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_execution_role_policy_attachment" {
     role = aws_iam_role.lambda_execution_role.name
-    policy_arn = "arn:aws:iam:aws:policy/service-role/${var.policy_name}"
-    
+    policy_arn = "arn:aws:iam::aws:policy/service-role/${var.policy_name}"
+  
 }
